2520ec59286467b6a9fe21da8faa720afcc26b7f
tahaimage8 <tahaimage8@gmail.com>
2026-08-21 21:05:34 +0600

    Added median solution

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -109,7 +109,16 @@ let merged = [];
 
 
 function findMedian(nums){
-    // Write your code here
+if (!nums || nums.length === 0) return 0;
+
+    const sorted = [...nums].sort((a, b) => a - b);
+    const mid = Math.floor(sorted.length / 2);
+
+    if (sorted.length % 2 === 0) {
+        return (sorted[mid - 1] + sorted[mid]) / 2;
+    } else {
+        return sorted[mid];
+    }
 }
 
 
