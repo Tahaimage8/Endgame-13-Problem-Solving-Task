a77de2e2e9a05a9d601c6ba8a65ce612f72f403c
tahaimage8 <tahaimage8@gmail.com>
2026-08-21 21:04:02 +0600

    Added merge sorted arrays solution

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -66,9 +66,46 @@ function isPrime(num){
 
 
 
+
+
+
 function mergeSortedArrays(arr1, arr2){
-    // Write your code here
+let merged = [];
+    let i = 0;
+    let j = 0;
+
+    while(i < arr1.length && j < arr2.length){
+        if(arr1[i] < arr2[j]){
+            merged.push(arr1[i]);
+            i++;
+        }
+        else{
+            merged.push(arr2[j]);
+            j++;
+        }
+
+    }
+    while(i < arr1.length){
+        merged.push(arr1[i]);
+        i++;
+    }
+
+    while(j < arr2.length){
+        merged.push(arr2[j]);
+        j++;
+    }
+
+    return merged;
 }
+
+
+
+
+
+
+
+
+
 
 
 function findMedian(nums){
