c6ef477e1c0843b980f0b549ce9e5abf97e1d281
tahaimage8 <tahaimage8@gmail.com>
2026-08-21 20:50:55 +0600

    Added fibonacci solutions

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -5,8 +5,19 @@ function isLeapYear(year){
 }
 
 function generateFibonacci(n){
-  // Write your code here
+if (n <= 0) return [];
+    if (n === 1) return [0];
+    
+    const fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
+    return fib;
 }
+
+
+
+
 
 
 function findGCD(a, b){
