85a225db05119b83337a3c4eb240f31d40a24344
tahaimage8 <tahaimage8@gmail.com>
2026-08-21 21:00:18 +0600

    Added prime number solution

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -54,7 +54,14 @@ if (a === 0 || b === 0) return 0;
 
 
 function isPrime(num){
-    // Write your code here
+ if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+    return true;
 }
 
 
