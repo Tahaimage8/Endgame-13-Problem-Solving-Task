c596ae830c9ea04b4070cf422c85868e4b6dfbee
tahaimage8 <tahaimage8@gmail.com>
2026-08-30 18:10:37 +0600

    Reverse String

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -1,5 +1,11 @@
 function reverseString(str) {
+    let result = "";
 
+    for (let i = str.length - 1; i >= 0; i--) {
+        result = result + str[i];
+    }
+
+    return result;
 }
 
 function findMax(nums){
