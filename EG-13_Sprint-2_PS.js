8e1972e8b959804f6be2a17ca19b0857cca714f0
tahaimage8 <tahaimage8@gmail.com>
2026-08-30 18:27:47 +0600

    Added Count Vowels

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -47,8 +47,22 @@ function sumArray(nums) {
     return sum;
 }
 
-function countVowels(str){
-    // Write your code here
+function countVowels(str) {
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (
+            str[i] === "a" ||
+            str[i] === "e" ||
+            str[i] === "i" ||
+            str[i] === "o" ||
+            str[i] === "u"
+        ) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 
