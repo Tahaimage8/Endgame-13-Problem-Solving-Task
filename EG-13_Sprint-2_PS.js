343c8b704ae4b597c89cb8a90c279bd598e08522
tahaimage8 <tahaimage8@gmail.com>
2026-08-30 18:10:04 +0600

    Endgame-13 Sprint-2 Problem

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -1,9 +1,50 @@
 function reverseString(str) {
-    let result = "";
 
-    for (let i = str.length - 1; i >= 0; i--) {
-        result = result + str[i];
-    }
+}
 
-    return result;
+function findMax(nums){
+    // Write your code here
+}
+
+
+
+function isPalindrome(str){
+    // Write your code here
+}
+
+
+function sumArray(nums){
+    // Write your code here
+}
+
+function countVowels(str){
+    // Write your code here
+}
+
+
+function twoSum(nums, target){
+    // Write your code here
+}
+
+
+function flattenArray(arr){
+    // Write your code here
+
+}
+
+
+function groupAnagrams(strs){
+    // Write your code here
+}
+
+
+
+function lengthOfLongestSubstring(s){
+    // Write your code here
+}
+
+
+
+function deepClone(obj){
+    // Write your code here
 }
