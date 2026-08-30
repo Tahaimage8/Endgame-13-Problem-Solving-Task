3a6e36de3d1c3205beac1a2134228f990997ad8c
tahaimage8 <tahaimage8@gmail.com>
2026-08-30 18:32:54 +0600

    Added Two Sum

--- EG-13_Sprint-2_PS.js
+++ EG-13_Sprint-2_PS.js
@@ -66,8 +66,18 @@ function countVowels(str) {
 }
 
 
-function twoSum(nums, target){
-    // Write your code here
+function twoSum(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+
+        for (let j = i + 1; j < nums.length; j++) {
+
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+
+    return [];
 }
 
 
